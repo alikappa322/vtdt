75b211f0521a940af16e5e0c4361f8068e25aee1
alikappa322 <34668402+alikappa322@users.noreply.github.com>
2017-12-23 23:53:33 +0600

    Update prosto tak.py

--- prosto tak.py
+++ prosto tak.py
@@ -1,22 +1,17 @@
-import random
-a1 = [random.randrange(50) for i in range(20)]
-print(a1)
-
-b1 = [random.randrange(50) for i in range(20)]
-print(b1)
-k = int(input("chislo: "))
-des = 10
-def func(mas,k_d):
+    # 0 1 2 3 4 5   6  7   8  9 10 11  12  13  14  15  16  17  18
+a1 = [5,4,3,1,2,1, 22, 31,48, 2, 1,99, 42, 22, 67, 82, 15, 61, 67]
+b1 = [1, 4, 2, 6, 3, 22, 66, 33, 82, 11, 2, 3, 4, 5]
+def func(X):
+    a_max = 0
     i_max = 0
-    mas_max = 0
-    for i in range(len(mas)):
-        if mas[i]>=mas_max:
-            mas_max = mas[i]
+    k = 0.5
+    for i in range(len(a1)):
+        if a1[i]>a_max:
+            a_max = a1[i]
             i_max = i
-    if k_d not in mas:
-        mas[i_max] = k_d
-        print(mas)
-    else:
-        print("число {} уже есть".format(k_d))
-func(a1,k)
-func(b1,des)
+    for n in range(len(a1)):
+        if n > i_max:
+            a1[n] = 0.5
+    print(X)
+func(a1)
+func(b1)
