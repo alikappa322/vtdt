44f41fe3e819daf8452fe3c16518b75b0a63e02c
alikappa322 <34668402+alikappa322@users.noreply.github.com>
2017-12-24 00:13:54 +0600

    Update prosto tak.py

--- prosto tak.py
+++ prosto tak.py
@@ -1,17 +1,24 @@
     # 0 1 2 3 4 5   6  7   8  9 10 11  12  13  14  15  16  17  18
+import random
+a = [random.randrange(50) for i in range(20)]
+print("массив через рандом: ",a)
 a1 = [5,4,3,1,2,1, 22, 31,48, 2, 1,99, 42, 22, 67, 82, 15, 61, 67]
 b1 = [1, 4, 2, 6, 3, 22, 66, 33, 82, 11, 2, 3, 4, 5]
 def func(X):
     a_max = 0
     i_max = 0
     k = 0.5
-    for i in range(len(a1)):
-        if a1[i]>a_max:
-            a_max = a1[i]
+    for i in range(len(X)):
+        if X[i]>a_max:
+            a_max = X[i]
             i_max = i
-    for n in range(len(a1)):
+    for n in range(len(X)):
         if n > i_max:
-            a1[n] = 0.5
+            X[n] = k
     print(X)
+print("massiv a1")
 func(a1)
+print("b1")
 func(b1)
+print("huinia")
+func(a)
