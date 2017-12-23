c96f8e48f643f8108f08173847aadae9f684644e
alikappa322 <34668402+alikappa322@users.noreply.github.com>
2017-12-23 23:50:21 +0600

    Update prosto tak.py

--- prosto tak.py
+++ prosto tak.py
@@ -1,13 +1,16 @@
-a1 = [1,22,31,2,1,42,22,1000,67,82,15,61,67]
+import random
+a1 = [random.randrange(50) for i in range(20)]
+print(a1)
 
-b1 = [1,4,2,6,3,22,66,33,82,11,2,3,4,5,123415]
+b1 = [random.randrange(50) for i in range(20)]
+print(b1)
 k = int(input("chislo: "))
 des = 10
 def func(mas,k_d):
     i_max = 0
     mas_max = 0
     for i in range(len(mas)):
-        if mas[i]>mas_max:
+        if mas[i]>=mas_max:
             mas_max = mas[i]
             i_max = i
     if k_d not in mas:
