764686d116e51d17db9a3d48f257d21cf9ce2cd1
alikappa322 <34668402+alikappa322@users.noreply.github.com>
2017-12-26 08:52:37 +0600

    Update 435.py

--- 435.py
+++ 435.py
@@ -8,15 +8,3 @@ k = int(input("chislo: "))
 des = 10
 def func(mas,k_d):
     i_max = 0
-    mas_max = 0
-    for i in range(len(mas)):
-        if mas[i]>=mas_max:
-            mas_max = mas[i]
-            i_max = i
-    if k_d not in mas:
-        mas[i_max] = k_d
-        print(mas)
-    else:
-        print("число {} уже есть".format(k_d))
-func(a1,k)
-func(b1,des)
