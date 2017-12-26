f4cb733d170a9b6d970c7555e0ac9f8a7a32df1e
alikappa322 <34668402+alikappa322@users.noreply.github.com>
2017-12-26 08:53:07 +0600

    Add files via upload

--- vt12.py
+++ vt12.py
@@ -1,23 +1,12 @@
-import random
-a = [random.randrange(50) for i in range(20)]
-print("массив через рандом: ",a)
-a1 = [5,4,3,1,2,1, 22, 31,48, 2, 1,99, 42, 22, 67, 82, 15, 61, 67]
-b1 = [1, 4, 2, 6, 3, 22, 66, 33, 82, 11, 2, 3, 4, 5]
-def func(X):
-    a_max = 0
-    i_max = 0
-    k = 0.5
-    for i in range(len(X)):
-        if X[i]>a_max:
-            a_max = X[i]
+    mas_max = 0
+    for i in range(len(mas)):
+        if mas[i]>=mas_max:
+            mas_max = mas[i]
             i_max = i
-    for n in range(len(X)):
-        if n > i_max:
-            X[n] = k
-    print(X)
-print("massiv a1")
-func(a1)
-print("b1")
-func(b1)
-print("huinia")
-func(a)
+    if k_d not in mas:
+        mas[i_max] = k_d
+        print(mas)
+    else:
+        print("число {} уже есть".format(k_d))
+func(a1,k)
+func(b1,des)
